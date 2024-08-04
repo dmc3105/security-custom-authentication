6ac5e019818be4474a699aae36a7d42c70aa9c05
Говоров А. В <algovor04@gmail.com>
2024-08-04 14:57:14 +0700

    Init commit

---
+++ src/test/java/com/dmc3105/customauthentication/CustomAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmc3105.customauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
