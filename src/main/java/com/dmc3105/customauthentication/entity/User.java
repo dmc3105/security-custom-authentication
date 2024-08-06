0ea0a2e3a361c52d5c0d6b3d41062ddfb26a5d63
Говоров А. В <algovor04@gmail.com>
2024-08-07 00:38:01 +0700

    Add User class

---
+++ src/main/java/com/dmc3105/customauthentication/entity/User.java
@@ -0,0 +1,37 @@
+package com.dmc3105.customauthentication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Data
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String email;
+    private String password;
+    private String firstname;
+    private String lastname;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
