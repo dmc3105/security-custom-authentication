31055e9a2e37c4853e66009045610fea21f5a4b5
Говоров А. В <algovor04@gmail.com>
2024-08-07 00:36:59 +0700

    Add HexAuthenticationToken class

---
+++ src/main/java/com/dmc3105/customauthentication/service/HexAuthenticationToken.java
@@ -0,0 +1,24 @@
+package com.dmc3105.customauthentication.service;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.codec.Hex;
+
+import java.nio.charset.StandardCharsets;
+
+public class HexAuthenticationToken extends UsernamePasswordAuthenticationToken {
+    public HexAuthenticationToken(String hexToken) {
+        super(extractUsername(hexToken), extractPassword(hexToken));
+    }
+
+    private static String extractUsername(String hexToken) {
+        byte[] bytes = Hex.decode(hexToken);
+        String decodedToken = new String(bytes, StandardCharsets.UTF_8);
+        return decodedToken.split(":")[0];
+    }
+
+    private static String extractPassword(String hexToken) {
+        byte[] bytes = Hex.decode(hexToken);
+        String decodedToken = new String(bytes, StandardCharsets.UTF_8);
+        return decodedToken.split(":")[1];
+    }
+}
