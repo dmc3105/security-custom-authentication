728df0a717f4b8f4b3d381fd1cb7d772e838b888
Говоров А. В <algovor04@gmail.com>
2024-08-07 00:42:24 +0700

    Update SecurityConfig
    - Add HexAuthenticationFilter bean
    - Add AuthenticationManager bean
    - Add PasswordEncoder bean
    - Add HexAuthenticationFilter in SecurityFilterChain

---
+++ src/main/java/com/dmc3105/customauthentication/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.dmc3105.customauthentication.security;
+
+import com.dmc3105.customauthentication.service.HexAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, HexAuthenticationFilter hexAuthenticationFilter) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("api/v1/auth/**").permitAll()
+                        .requestMatchers("public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(hexAuthenticationFilter, BasicAuthenticationFilter.class);
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public HexAuthenticationFilter hexAuthenticationFilter(AuthenticationManager authenticationManager) {
+        return new HexAuthenticationFilter(authenticationManager);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
